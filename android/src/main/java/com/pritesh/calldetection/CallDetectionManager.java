6619868c409b4b6100d0d31e6f011fc67835e762
Nicoleta Mazarel <nicoleta.mazarel@ivfuture.uk>
2025-11-07 11:45:34 +0200

    fix: build error from missing CallStateUpdateActionModule
    - Remove CallStateUpdateActionModule reference from CallDetectionManager.java
    - Update build.gradle with safeExtGet for proper version handling
    - Update package.json peerDependencies for React Native 0.72+ compatibility

--- android/src/main/java/com/pritesh/calldetection/CallDetectionManager.java
+++ android/src/main/java/com/pritesh/calldetection/CallDetectionManager.java
@@ -1,38 +1,25 @@
 package com.pritesh.calldetection;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Arrays;
-
 
 public class CallDetectionManager implements ReactPackage {
 
     @Override
-    public List<NativeModule> createNativeModules(
-            ReactApplicationContext reactContext) {
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
-        CallDetectionManagerModule callDetectionModule = new CallDetectionManagerModule(reactContext);
-        modules.add(callDetectionModule);
-
+        modules.add(new CallDetectionManagerModule(reactContext));
         return modules;
     }
 
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Arrays.<Class<? extends JavaScriptModule>>asList(
-                CallStateUpdateActionModule.class
-        );
-    }
-
     @Override
-    public List<ViewManager> createViewManagers(
-            ReactApplicationContext reactContext) {
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
-
 }
